456d567c1d16c160c49b862a57b7dade97d3660d
ryanparketh <ryanpark@rocketmail.com>
2022-12-15 09:45:34 -0500

    added userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  updateUser,
+} = require('../../controllers/userController');
+
+// /api/user
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend).post(addFriend);
+
+module.exports = router;
