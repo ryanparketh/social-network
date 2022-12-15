e06e9a4d297bbed712535fb73c489e4d8f667dca
ryanparketh <ryanpark@rocketmail.com>
2022-12-15 09:17:52 -0500

    finished User.js

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },],
+
+    friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Thought',
+    },],
+
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
